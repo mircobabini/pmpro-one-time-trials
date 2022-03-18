20f99f23f8fd7c3fece20034caadfb690a5fb54d
Mirco Babini <mirkolofio@gmail.com>
2022-03-18 17:02:43 +0100

    Check if is_numeric. Non numeric does not mean trial.

--- includes/addons/pmprosd.php
+++ includes/addons/pmprosd.php
@@ -3,9 +3,8 @@
  * This snippet should be moved to:
  * https://github.com/strangerstudios/pmpro-subscription-delays
  *
- * TODO this will become useless as soon as PR35 is merged
- * https://github.com/strangerstudios/pmpro-subscription-delays/pull/35
- * 
+ * TODO Update: we will not merge because https://github.com/strangerstudios/pmpro-subscription-delays/pull/35#issuecomment-1072541697
+ *
  * @param array $trial_levels
  *
  * @returns array
@@ -14,7 +13,7 @@ add_filter( 'pmproott_trial_levels', function ( $trial_levels ) {
 	$levels = pmpro_getAllLevels( true, true );
 	foreach ( $levels as $level ) {
 		$delay = get_option( 'pmpro_subscription_delay_' . $level->id, '' );
-		if ( ! empty( $delay ) ) {
+		if ( ! empty( $delay ) && is_numeric( $delay ) ) {
 			$trial_levels[ (int) $level->id ] = $level;
 		}
 	}
